4d81eafe2cf10d4ac0cf0d1d47207505d1ebb967
jashshah-2103 <59500125+jashshah-2103@users.noreply.github.com>
2023-05-15 19:46:43 +0530

    Update assn4a.cu

--- assn4a.cu
+++ assn4a.cu
@@ -13,6 +13,111 @@
 // ./a.out
 
 %%cu
+#include<iostream>
+#include<bits/stdc++.h>
+#include<cuda.h>
+#define BLOCK_SIZE 64
+using namespace std;
+
+void fill_array(int *arr,int size){
+    for(int i = 0;i < size; i++){
+        arr[i] = rand() % 100;
+    }
+}
+
+void add_cpu(int *arr1, int *arr2, int *result, int size){
+    for(int i = 0;i < size; i++){
+        result[i] = arr1[i] + arr2[i];
+    }
+}
+
+void print_matrix(int *arr, int size){
+    for(int i = 0; i < size; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+__global__ void add(int *arr1, int *arr2, int *arr3,int size){
+    int block_id = blockIdx.x * blockDim.x + threadIdx.x;
+    if(block_id < size){
+        arr3[block_id] = arr1[block_id] + arr2[block_id];
+    }
+}
+
+int main(){
+    int *arr1_cpu,*arr2_cpu,*result_cpu;
+    int size;
+    cout << "Enter size of vector: ";
+    cin >> size;
+
+    arr1_cpu = new int[size];
+    arr2_cpu = new int[size];
+    result_cpu = new int[size];
+
+    fill_array(arr1_cpu,size);
+    cout << "Array 1: ";
+    // print_matrix(arr1_cpu,size);
+    fill_array(arr2_cpu,size);
+    cout << "Array 2: ";
+    // print_matrix(arr2_cpu,size);
+
+    int *arr1_gpu,*arr2_gpu,*result_gpu;
+    
+    cudaMallocManaged(&arr1_gpu, size * sizeof(int));
+    cudaMallocManaged(&arr2_gpu, size * sizeof(int));
+    cudaMallocManaged(&result_gpu, size * sizeof(int));
+
+    cudaMemcpy(arr1_gpu,arr1_cpu,size * sizeof(int),cudaMemcpyHostToDevice);
+    cudaMemcpy(arr2_gpu,arr2_cpu,size * sizeof(int),cudaMemcpyHostToDevice);
+    cudaEvent_t start,stop;
+    float elapsedTime;
+    
+    dim3 dimGrid(size / BLOCK_SIZE);
+    dim3 dimBlock(BLOCK_SIZE);
+
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start,0);
+
+    add<<<dimGrid,dimBlock>>>(arr1_gpu,arr2_gpu,result_gpu,size);
+    cudaEventRecord(stop,0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsedTime,start,stop);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+    cudaMemcpy(result_cpu,result_gpu,size * sizeof(int),cudaMemcpyDeviceToHost);
+    cout << "GPU result:\n";
+    // print_matrix(result_cpu,size);
+    cout<<"Elapsed Time = "<<elapsedTime<<" milliseconds" << endl;
+    cudaFree(arr1_gpu);
+    cudaFree(arr2_gpu);
+    cudaFree(result_gpu);
+
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start,0);
+
+    add_cpu(arr1_cpu,arr2_cpu,result_cpu,size);
+    cudaEventRecord(stop,0);
+    cudaEventSynchronize(stop);
+    cudaEventElapsedTime(&elapsedTime,start,stop);
+    cudaEventDestroy(start);
+    cudaEventDestroy(stop);
+    cout << "CPU result:\n";
+    // print_matrix(result_cpu,size);
+    cout<<"Elapsed Time = "<<elapsedTime<<" milliseconds" << endl;
+
+    return 0;
+}
+
+//__________________________________________________________________________________________________________________
+
+
+
+
+
+%%cu
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda_runtime.h>
@@ -198,71 +303,5 @@ int main()
 //     free(h_b);
 //     free(h_c);
  
-//     return 0;
-// }
-// #include <iostream>
-// #include <cstdlib>
-// #include <ctime>
-// #include <omp.h>
-
-// using namespace std;
-
-// const int VECTOR_SIZE = 100;
-// int main()
-// {
-//     // initialize random seed
-//     srand(time(NULL));
-//     // allocate memory for the vectors 
-//     int* vector1 = new int[VECTOR_SIZE]; 
-//     int* vector2 = new int[VECTOR_SIZE]; 
-//     int* result = new int[VECTOR_SIZE];
-//     // fill the vectors with random numbers 
-//     #pragma omp parallel for
-//     for (int i = 0; i < VECTOR_SIZE; i++)
-//     {
-//         vector1[i] = rand() % 10000;
-//         vector2[i] = rand() % 10000;
-//     }
-//     // add the vectors in parallel using OpenMP 
-//     #pragma omp parallel for
-//     for (int i = 0; i < VECTOR_SIZE; i++)
-//     {
-//         result[i] = vector1[i] + vector2[i];
-//     }
-//     // print the first and second vectors and their sum 
-//     cout << "Vector 1: \n[";
-//     for (int i = 0; i < VECTOR_SIZE; i++)
-//     {
-//         cout << vector1[i];
-//         if (i != VECTOR_SIZE - 1)
-//         {
-//             cout << ", ";
-//         }
-//     }
-//     cout << "]" << endl;
-//     cout << "Vector 2:\n [";
-//     for (int i = 0; i < VECTOR_SIZE; i++)
-//     {
-//         cout << vector2[i];
-//         if (i != VECTOR_SIZE - 1)
-//         {
-//             cout << ", ";
-//         }
-//     }
-//     cout << "]" << endl;
-//     cout << "Result: \n[";
-//     for (int i = 0; i < VECTOR_SIZE; i++)
-//     {
-//         cout << result[i];
-//         if (i != VECTOR_SIZE - 1)
-//         {
-//             cout << ", ";
-//         }
-//     }
-//     cout << "]" << endl;
-//     // free the allocated memory
-//     delete[] vector1;
-//     delete[] vector2;
-//     delete[] result;
 //     return 0;
 // }
